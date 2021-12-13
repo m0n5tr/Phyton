d01026835ad4a61625648bccbc3bf5a039172f50
m0n5tr <m0n5tr@users.noreply.github.com>
2021-12-13 17:38:18 +0300

    add save last error file time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import shelve
 from tkinter import *
+
+KEY_LAST_TIME = "time"
 
 MIN_FILE_SIZE = 50
 EXTENSION = '.txt'
@@ -19,14 +22,26 @@ class ErrorFile:
 
 
 def print_hi():
+    sh = shelve
     try:
         all_files = os.listdir(DIRECTORY)  # список файлов
         need_files = filter(lambda x: x.endswith(EXTENSION), all_files)  # фильтр
         error_files = set()
+        sh = shelve.open(DIRECTORY + "\\" + "last_error_file", "c")
+        try:
+            saved_time = sh[KEY_LAST_TIME]
+        except KeyError:
+            saved_time = 0.0
+        last_saved_time = saved_time
         for f in need_files:  # цикл по фильтру
-            size = os.path.getsize(DIRECTORY + "\\" + f)  # считывание объёма
+            file = DIRECTORY + "\\" + f
+            size = os.path.getsize(file)  # считывание объёма
+            c_time = os.path.getctime(file)
             if size < MIN_FILE_SIZE:
-                error_files.add(ErrorFile(f, str(size)))
+                if c_time > saved_time:
+                    error_files.add(ErrorFile(f, str(size)))
+                    if last_saved_time < c_time:
+                        last_saved_time = c_time
         if len(error_files) > 0:
             root = Tk()
             root.title("Ошибка файла")
@@ -36,9 +51,11 @@ def print_hi():
                 files_list.insert(END, f.print())
             files_list.pack()
             root.mainloop()
-
+            sh[KEY_LAST_TIME] = last_saved_time
     except FileNotFoundError:  # обработка ошибок
         print("Путь не найден: " + DIRECTORY)
+    finally:
+        sh.close()
 
 
 if __name__ == '__main__':
